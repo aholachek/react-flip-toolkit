06f4cb1155ad2451c80721b941e5e4b312e88d7a
Alex Holachek <aholachek@gmail.com>
2018-06-19 21:38:29 -0400

    umd take 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-export { default as Flipper } from "./Flipper"
-export { default as Flipped } from "./Flipped"
+import Flipper from "./Flipper"
+import Flipped from "./Flipped"
 
 // for umd build
 export default {
   Flipper,
   Flipped
 }
+
+export { default as Flipper } from "./Flipper"
+export { default as Flipped } from "./Flipped"
