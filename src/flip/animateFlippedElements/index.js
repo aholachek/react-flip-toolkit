7e5d3b6a4a26ffb7ade50c1d46a91fdd77f329e6
Alex Holachek <aholachek@gmail.com>
2018-11-13 13:28:22 -0500

    add persistent rotate

--- src/flip/animateFlippedElements/index.js
+++ src/flip/animateFlippedElements/index.js
@@ -69,10 +69,13 @@ export const createApplyStylesFunc = ({ element, invertedChildren, body }) => ({
   if (!matrix) return
   let stringTransform = convertMatrix2dArrayToString(matrix)
 
-  // saw some weird behavior when the identity transform was left on the element
-  // on a table
+  // keep an invisible transform on the element to try to
+  // prevent Chrome from pixel-snapping when scale transforms
+  // are removed
   stringTransform =
-    stringTransform === "matrix(1, 0, 0, 1, 0, 0)" ? "" : stringTransform
+    stringTransform === "matrix(1, 0, 0, 1, 0, 0)"
+      ? "rotateY(.01)"
+      : stringTransform
   element.style.transform = stringTransform
 
   if (invertedChildren) {
