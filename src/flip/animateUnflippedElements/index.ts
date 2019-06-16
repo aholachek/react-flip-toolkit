7db30f446dcaa0819dc7faba3c9415ffe191714d
aholachek <aholachek@gmail.com>
2019-06-15 23:35:20 -0400

    temp

--- src/flip/animateUnflippedElements/index.ts
+++ src/flip/animateUnflippedElements/index.ts
@@ -22,14 +22,6 @@ const animateUnflippedElements = ({
       flipCallbacks[id].onExit
   )
 
-  // make sure appearing elements aren't taken into account by the filterFlipDescendants function
-  enteringElementIds.forEach(id => {
-    const element = getElement(id)
-    if (element) {
-      element.dataset.isAppearing = 'true'
-    }
-  })
-
   const hideEnteringElements = () => {
     animatedEnteringElementIds.forEach(id => {
       const element = getElement(id)
