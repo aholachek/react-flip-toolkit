d123409e80ea5add09d3d675ecfe29fb104efbf5
Nate Wienert <natewienert@gmail.com>
2019-06-18 21:54:27 -0700

    Export Spring for external use in onExit/onAppear

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import Flipper from './Flipper'
 import Flipped from './Flipped'
 import ExitContainer from './ExitContainer'
+import SpringSystem from './forked-rebound'
 
 // for umd build
 export default {
   Flipper,
   Flipped,
-  ExitContainer
+  ExitContainer,
+  SpringSystem
 }
 
 export { default as Flipper } from './Flipper'
 export { default as Flipped } from './Flipped'
 export { default as ExitContainer } from './ExitContainer'
+export { default as SpringSystem } from './forked-rebound'
