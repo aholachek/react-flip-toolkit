0312217fa423e1e6073484c262179ec1f9598508
aholachek <aholachek@gmail.com>
2019-09-05 14:48:35 -0400

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended'
   ],
