77d3096ce93b34755a87e999c0e19455d4a855e6
Alex Holachek <aholachek@gmail.com>
2020-06-11 11:44:00 -0400

    move mocha tests

---
+++ packages/flip-toolkit/mocha/tests.js
@@ -0,0 +1,5 @@
+import '../src/flip/animateFlippedElements/__tests__/applyStyles.domtest.js'
+import '../src/flip/animateFlippedElements/__tests__/animateFlippedElements.domtest.js'
+import '../src/flip/animateUnflippedElements/animateUnflippedElements.domtest.js'
+import '../src/flip/getFlippedElementPositions/getFlippedElementPositions.domtest.js'
+mocha.run() // eslint-disable-line
