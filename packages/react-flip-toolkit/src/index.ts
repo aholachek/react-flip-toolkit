e40e36db57468b79e21d70451ca468327ea474a4
Stefano Baldan <stefano@miro.com>
2024-07-02 17:44:13 +0200

    Re-exporting global animation switch functions in react-flip-toolkit

--- packages/react-flip-toolkit/src/index.ts
+++ packages/react-flip-toolkit/src/index.ts
@@ -1,3 +1,4 @@
+export { disableFlip, enableFlip, isFlipEnabled } from 'flip-toolkit'
 export { default as Flipper } from './Flipper'
 export { default as Flipped } from './Flipped'
 export { default as ExitContainer } from './ExitContainer'
